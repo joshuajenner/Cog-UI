6ccd3e911ed014d574a913be62058ff3ec639dde
Joshua Jenner <joshua-jenner@hotmail.com>
2023-02-19 18:10:27 +0800

    simplified input manager

--- addons/bolt_ui/autoloads/input_map_manager.gd
+++ addons/bolt_ui/autoloads/input_map_manager.gd
@@ -4,31 +4,26 @@ const INPUT_FILE_PATH : String = "user://user_input_map.tres"
 
 
 func _ready():
-	load_resource_file()
-
-
-func load_resource_file() -> void:
 	var file_exists : bool = ResourceLoader.exists(INPUT_FILE_PATH)
 	
 	if file_exists:
-		var file : InputMapResource = ResourceLoader.load(INPUT_FILE_PATH)
-		load_resource_to_settings(file)
+		load_resource_to_input_map()
 	else:
-		save_settings_to_resource()
+		save_input_map_to_resource()
 
 
-func load_resource_to_settings(file : InputMapResource) -> void:
-	var actions_map : Array[StringName] = InputMap.get_actions()
-	var file_input_map : Dictionary = file.input_map
+func load_resource_to_input_map() -> void:
+	var resource : InputMapResource = ResourceLoader.load(INPUT_FILE_PATH)
+	var user_map : Dictionary = resource.input_map
+	var game_map : Array[StringName] = InputMap.get_actions()
 	
-	for action in actions_map:
+	for action in game_map:
 		InputMap.action_erase_events(action)
 		
-		for key in file_input_map[action]:
-			InputMap.action_add_event(action, key)
+		for event in user_map[action]:
+			InputMap.action_add_event(action, event)
 
 
-func save_settings_to_resource() -> void:
-	var input_resource = InputMapResource.new()
-	input_resource.load_input_map()
+func save_input_map_to_resource() -> void:
+	var input_resource : InputMapResource = InputMapResource.new()
 	ResourceSaver.save(input_resource, INPUT_FILE_PATH)
