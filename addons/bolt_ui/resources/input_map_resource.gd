a4ec42da95fd0dd97cdcdcf6815b2ef1d2b447aa
Joshua Jenner <joshua-jenner@hotmail.com>
2023-02-19 18:10:44 +0800

    moved load to init and added typing

--- addons/bolt_ui/resources/input_map_resource.gd
+++ addons/bolt_ui/resources/input_map_resource.gd
@@ -2,10 +2,10 @@ class_name InputMapResource
 extends Resource
 
 
-@export var input_map = {}
+@export var input_map : Dictionary = {}
 
 
-func load_input_map():
+func _init():
 	var project_settings_input_map : Array[StringName] = InputMap.get_actions()
 	for action in project_settings_input_map:
 		input_map[action] = InputMap.action_get_events(action)
