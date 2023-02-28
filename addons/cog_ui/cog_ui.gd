7829e556437d04ea011b3d80928e3cdca1341879
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-02-28 14:16:06 +0800

    added audio bus autoload to plugin

--- addons/cog_ui/cog_ui.gd
+++ addons/cog_ui/cog_ui.gd
@@ -6,12 +6,14 @@ enum {
 	path
 }
 
-const input_map : Array[String] = ["InputMapManager", "res://addons/cog_ui/autoloads/input_map_manager.gd"]
-
+const input_map_manager : Array[String] = ["InputMapManager", "res://addons/cog_ui/autoloads/input_map_manager.gd"]
+const audio_bus_manager : Array[String] = ["AudioBusManager", "res://addons/cog_ui/autoloads/audio_bus_manager.gd"]
 
 func _enter_tree():
-	add_autoload_singleton(input_map[auto_name], input_map[path])
+	add_autoload_singleton(input_map_manager[auto_name], input_map_manager[path])
+	add_autoload_singleton(audio_bus_manager[auto_name], audio_bus_manager[path])
 
 
 func _exit_tree():
-	remove_autoload_singleton(input_map[auto_name])
+	remove_autoload_singleton(input_map_manager[auto_name])
+	remove_autoload_singleton(audio_bus_manager[auto_name])
