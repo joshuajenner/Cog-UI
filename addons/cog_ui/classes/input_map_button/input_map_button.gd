3b8d6d782c5b549f9a7e8f482b2e2373e86f3a2f
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-03-27 14:01:22 +0800

    Update input_map_button.gd

--- addons/cog_ui/classes/input_map_button/input_map_button.gd
+++ addons/cog_ui/classes/input_map_button/input_map_button.gd
@@ -1,38 +1,38 @@
 class_name InputMapButton
 extends Button
 
-# Seems like settings isn't being saved sometimes????
-
 @export var action : String
 @export var event_index : int = 0 
 
 const INPUT_REQUEST : String = "Press any key"
 
-var key_label : String = ""
+var event_label : String = ""
 var waiting_for_assign : bool = false
 
 
 func _ready():
 	if InputMap.has_action(action):
-		get_inputs_from_action()
+		get_events_from_action()
 		pressed.connect(_on_pressed)
 	else:
 		disabled = true
 
 
-func get_inputs_from_action():
-	var keys = InputMap.action_get_events(action)
-	if keys.size() >= 1:
-		key_label = keys[event_index].as_text_keycode()
+func get_events_from_action():
+	var events = InputMap.action_get_events(action)
+	if events.size() > event_index:
+		event_label = events[event_index].as_text_keycode()
 		disabled = false
-		
-	text = key_label
+	elif events.size() < event_index:
+		disabled = true
+	
+	text = event_label
 
 
 func _input(event):
 	if waiting_for_assign:
 		if event.is_action_pressed("ui_cancel"):
-			text = key_label
+			text = event_label
 			waiting_for_assign = false
 		elif event is InputEventKey and event.is_pressed():
 			var key_code = event.as_text_keycode()
@@ -42,14 +42,14 @@ func _input(event):
 
 
 func assign_key_to_action(new_event: InputEvent):
-	var assigned_events = InputMap.action_get_events(action)
-	if assigned_events.size() <= event_index:
+	var old_events = InputMap.action_get_events(action)
+	if old_events.size() <= event_index:
 		InputMap.action_add_event(action, new_event)
 	else:
 		InputMap.action_erase_events(action)
-		for event in range(0, assigned_events.size()-1):
+		for event in range(0, old_events.size()):
 			if event != event_index:
-				InputMap.action_add_event(action, assigned_events[event])
+				InputMap.action_add_event(action, old_events[event])
 			else:
 				InputMap.action_add_event(action, new_event)
 		
