72692b58a5c5894e41a90ae20f230aa6126442ce
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-03-31 13:05:35 +0800

    removed in progress features

--- addons/cog_ui/cog_ui.gd
+++ addons/cog_ui/cog_ui.gd
@@ -6,14 +6,11 @@ enum {
 	path
 }
 
-const input_map_manager : Array[String] = ["InputMapManager", "res://addons/cog_ui/autoloads/input_map_manager.gd"]
 const audio_bus_manager : Array[String] = ["AudioBusManager", "res://addons/cog_ui/autoloads/audio_bus_manager.gd"]
 
 func _enter_tree():
-	add_autoload_singleton(input_map_manager[auto_name], input_map_manager[path])
 	add_autoload_singleton(audio_bus_manager[auto_name], audio_bus_manager[path])
 
 
 func _exit_tree():
-	remove_autoload_singleton(input_map_manager[auto_name])
 	remove_autoload_singleton(audio_bus_manager[auto_name])
--- addons/cog_ui/resources/event_key_labels.gd
+++
@@ -1,7 +0,0 @@
-class_name EventKeyLabels
-extends Resource
-
-
-var labels : Dictionary = {
-	KEY_D : "Dee"
-}
