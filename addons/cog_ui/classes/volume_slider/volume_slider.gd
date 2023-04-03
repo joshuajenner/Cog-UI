423ab9d090ea375537733cf195b0136c6dceea39
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-04-03 13:06:51 +0800

    init

--- addons/cog_ui/classes/volume_slider/volume_slider.gd
+++ addons/cog_ui/classes/volume_slider/volume_slider.gd
@@ -2,7 +2,7 @@
 class_name VolumeSlider
 extends HSlider
 
-@export var audio_bus : String :
+var audio_bus : String :
 	set = _on_audio_bus_changed
 
 var bus_index : int = 0
@@ -12,6 +12,28 @@ func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(audio_bus)
 	if bus_index != -1:
 		get_volume_from_bus()
+
+
+func _get_property_list() -> Array:
+	bus_index = AudioServer.get_bus_index(audio_bus)
+	update_configuration_warnings()
+	return [
+		{
+			name = "audio_bus",
+			type = TYPE_STRING,
+			hint = PROPERTY_HINT_ENUM,
+			hint_string = ",".join(get_audio_buses())
+		},
+	]
+
+
+func get_audio_buses() -> Array[String]:
+	var bus_count : int = AudioServer.bus_count
+	var buses : Array[String] = []
+	for index in bus_count:
+		buses.push_back(AudioServer.get_bus_name(index))
+	return buses
+
 
 
 func get_volume_from_bus() -> void:
