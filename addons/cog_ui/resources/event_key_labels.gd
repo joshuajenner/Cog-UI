f7b67e8f87510e9a3069b3d74e42e97ba64a8bf1
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-04-04 18:13:58 +0800

    finished custom label resource

--- addons/cog_ui/resources/event_key_labels.gd
+++ addons/cog_ui/resources/event_key_labels.gd
@@ -2,6 +2,136 @@ class_name EventKeyLabels
 extends Resource
 
 
+@export_group("Character")
+@export_subgroup("Letter")
+@export var _A : String = ""
+@export var _B : String = ""
+@export var _C : String = ""
+@export var _D : String = ""
+@export var _E : String = ""
+@export var _F : String = ""
+@export var _G : String = ""
+@export var _H : String = ""
+@export var _I : String = ""
+@export var _J : String = ""
+@export var _K : String = ""
+@export var _L : String = ""
+@export var _M : String = ""
+@export var _N : String = ""
+@export var _O : String = ""
+@export var _P : String = ""
+@export var _Q : String = ""
+@export var _R : String = ""
+@export var _S : String = ""
+@export var _T : String = ""
+@export var _U : String = ""
+@export var _V : String = ""
+@export var _W : String = ""
+@export var _X : String = ""
+@export var _Y : String = ""
+@export var _Z : String = ""
+@export_subgroup("Number")
+@export var _0 : String = ""
+@export var _1 : String = ""
+@export var _2 : String = ""
+@export var _3 : String = ""
+@export var _4 : String = ""
+@export var _5 : String = ""
+@export var _6 : String = ""
+@export var _7 : String = ""
+@export var _8 : String = ""
+@export var _9 : String = ""
+@export_subgroup("Special")
+@export var _SPACE : String = ""
+@export var _EXCLAM : String = ""
+@export var _QUOTEDBL : String = ""
+@export var _NUMBERSIGN : String = ""
+@export var _DOLLAR : String = ""
+@export var _PERCENT : String = ""
+@export var _AMPERSAND : String = ""
+@export var _APOSTROPHE : String = ""
+@export var _PARENLEFT : String = ""
+@export var _PARENRIGHT : String = ""
+@export var _ASTERISK : String = ""
+@export var _PLUS : String = ""
+@export var _COMMA : String = ""
+@export var _MINUS : String = ""
+@export var _PERIOD : String = ""
+@export var _SLASH : String = ""
+@export var _COLON : String = ""
+@export var _SEMICOLON : String = ""
+@export var _LESS : String = ""
+@export var _EQUAL : String = ""
+@export var _GREATER : String = ""
+@export var _QUESTION : String = ""
+@export var _AT : String = ""
+@export var _BRACKETLEFT : String = ""
+@export var _BACKSLASH : String = ""
+@export var _BRACKETRIGHT : String = ""
+@export var _ASCIICIRCUM : String = ""
+@export var _UNDERSCORE : String = ""
+@export var _QUOTELEFT : String = ""
+@export var _BRACELEFT : String = ""
+@export var _BAR : String = ""
+@export var _BRACERIGHT : String = ""
+@export var _ASCIITILDE : String = ""
+
+@export_group("Function")
+@export var _F1 : String = ""
+@export var _F2 : String = ""
+@export var _F3 : String = ""
+@export var _F4 : String = ""
+@export var _F5 : String = ""
+@export var _F6 : String = ""
+@export var _F7 : String = ""
+@export var _F8 : String = ""
+@export var _F9 : String = ""
+@export var _F10 : String = ""
+@export var _F11 : String = ""
+@export var _F12 : String = ""
+@export var _F13 : String = ""
+@export var _F14 : String = ""
+@export var _F15 : String = ""
+@export var _F16 : String = ""
+@export var _F17 : String = ""
+@export var _F18 : String = ""
+@export var _F19 : String = ""
+@export var _F20 : String = ""
+@export var _F21 : String = ""
+@export var _F22 : String = ""
+@export var _F23 : String = ""
+@export var _F24 : String = ""
+@export var _F25 : String = ""
+@export var _F26 : String = ""
+@export var _F27 : String = ""
+@export var _F28 : String = ""
+@export var _F29 : String = ""
+@export var _F30 : String = ""
+@export var _F31 : String = ""
+@export var _F32 : String = ""
+@export var _F33 : String = ""
+@export var _F34 : String = ""
+@export var _F35 : String = ""
+
+@export_group("Keypad")
+@export var _KP_MULTIPLY : String = ""
+@export var _KP_DIVIDE : String = ""
+@export var _KP_SUBTRACT : String = ""
+@export var _KP_PERIOD : String = ""
+@export var _KP_ADD : String = ""
+@export var _KP_ENTER : String = ""
+@export var _KP_0 : String = ""
+@export var _KP_1 : String = ""
+@export var _KP_2 : String = ""
+@export var _KP_3 : String = ""
+@export var _KP_4 : String = ""
+@export var _KP_5 : String = ""
+@export var _KP_6 : String = ""
+@export var _KP_7 : String = ""
+@export var _KP_8 : String = ""
+@export var _KP_9 : String = ""
+
+@export_group("Special")
 @export var _NONE : String = ""
 @export var _SPECIAL : String = ""
 @export var _ESCAPE  : String = ""
@@ -9,7 +139,6 @@ extends Resource
 @export var _BACKTAB : String = ""
 @export var _BACKSPACE : String = ""
 @export var _ENTER : String = ""
-@export var _KP_ENTER : String = ""
 @export var _INSERT : String = ""
 @export var _DELETE : String = ""
 @export var _PAUSE : String = ""
@@ -32,16 +161,445 @@ extends Resource
 @export var _NUMLOCK : String = ""
 @export var _SCROLLLOCK : String = ""
 
+@export_group("Media")
+@export var _BACK : String = ""
+@export var _FORWARD : String = ""
+@export var _STOP : String = ""
+@export var _REFRESH : String = ""
+@export var _VOLUMEDOWN : String = ""
+@export var _VOLUMEMUTE : String = ""
+@export var _VOLUMEUP : String = ""
+@export var _MEDIAPLAY : String = ""
+@export var _MEDIASTOP : String = ""
+@export var _MEDIAPREVIOUS : String = ""
+@export var _MEDIANEXT : String = ""
+@export var _MEDIARECORD : String = ""
 
-func get_label(keycode: int):
+@export_group("Launch")
+@export var _LAUNCHMAIL : String = ""
+@export var _LAUNCHMEDIA : String = ""
+@export var _LAUNCH0 : String = ""
+@export var _LAUNCH1 : String = ""
+@export var _LAUNCH2 : String = ""
+@export var _LAUNCH3 : String = ""
+@export var _LAUNCH4 : String = ""
+@export var _LAUNCH5 : String = ""
+@export var _LAUNCH6 : String = ""
+@export var _LAUNCH7 : String = ""
+@export var _LAUNCH8 : String = ""
+@export var _LAUNCH9 : String = ""
+@export var _LAUNCHA : String = ""
+@export var _LAUNCHB : String = ""
+@export var _LAUNCHC : String = ""
+@export var _LAUNCHD : String = ""
+@export var _LAUNCHE : String = ""
+@export var _LAUNCHF : String = ""
+
+@export_group("Others")
+@export var _MENU : String = ""
+@export var _HYPER : String = ""
+@export var _HELP : String = ""
+@export var _HOMEPAGE : String = ""
+@export var _FAVORITES : String = ""
+@export var _SEARCH : String = ""
+@export var _STANDBY : String = ""
+@export var _OPENURL : String = ""
+@export var _UNKNOWN : String = ""
+@export var _YEN : String = ""
+@export var _SECTION : String = ""
+@export var _GLOBE : String = ""
+@export var _KEYBOARD : String = ""
+@export var _JIS_EISU : String = ""
+@export var _JIS_KANA : String = ""
+
+
+func get_label(keycode: int) -> String:
 	match (keycode):
 		KEY_NONE:
-			return KEY_NONE
+			return _NONE
 		KEY_SPECIAL:
-			return KEY_SPECIAL
+			return _SPECIAL
 		KEY_ESCAPE:
-			return KEY_ESCAPE
+			return _ESCAPE
 		KEY_TAB:
-			return KEY_TAB
+			return _TAB
+		KEY_BACKTAB:
+			return _BACKTAB
+		KEY_BACKSPACE:
+			return _BACKSPACE
+		KEY_ENTER:
+			return _ENTER
+		KEY_KP_ENTER:
+			return _KP_ENTER
+		KEY_INSERT:
+			return _INSERT
+		KEY_DELETE:
+			return _DELETE
+		KEY_PAUSE:
+			return _PAUSE
+		KEY_PRINT:
+			return _PRINT
+		KEY_SYSREQ:
+			return _SYSREQ
+		KEY_CLEAR:
+			return _CLEAR
+		KEY_HOME:
+			return _HOME
+		KEY_END:
+			return _END
+		KEY_LEFT:
+			return _LEFT
+		KEY_UP:
+			return _UP
+		KEY_RIGHT:
+			return _RIGHT
+		KEY_DOWN:
+			return _DOWN
+		KEY_PAGEUP:
+			return _PAGEUP
+		KEY_PAGEDOWN:
+			return _PAGEDOWN
+		KEY_SHIFT:
+			return _SHIFT
+		KEY_CTRL:
+			return _CTRL
+		KEY_META:
+			return _META
+		KEY_ALT:
+			return _ALT
+		KEY_CAPSLOCK:
+			return _CAPSLOCK
+		KEY_NUMLOCK:
+			return _NUMLOCK
+		KEY_SCROLLLOCK:
+			return _SCROLLLOCK
+		KEY_F1:
+			return _F1
+		KEY_F2:
+			return _F2
+		KEY_F3:
+			return _F3
+		KEY_F4:
+			return _F4
+		KEY_F5:
+			return _F5
+		KEY_F6:
+			return _F6
+		KEY_F7:
+			return _F7
+		KEY_F8:
+			return _F8
+		KEY_F9:
+			return _F9
+		KEY_F10:
+			return _F10
+		KEY_F11:
+			return _F11
+		KEY_F12:
+			return _F12
+		KEY_F13:
+			return _F13
+		KEY_F14:
+			return _F14
+		KEY_F15:
+			return _F15
+		KEY_F16:
+			return _F16
+		KEY_F17:
+			return _F17
+		KEY_F18:
+			return _F18
+		KEY_F19:
+			return _F19
+		KEY_F20:
+			return _F20
+		KEY_F21:
+			return _F21
+		KEY_F22:
+			return _F22
+		KEY_F23:
+			return _F23
+		KEY_F24:
+			return _F24
+		KEY_F25:
+			return _F25
+		KEY_F26:
+			return _F26
+		KEY_F27:
+			return _F27
+		KEY_F28:
+			return _F28
+		KEY_F29:
+			return _F29
+		KEY_F30:
+			return _F30
+		KEY_F31:
+			return _F31
+		KEY_F32:
+			return _F32
+		KEY_F33:
+			return _F33
+		KEY_F34:
+			return _F34
+		KEY_F35:
+			return _F35
+		KEY_KP_MULTIPLY:
+			return _KP_MULTIPLY
+		KEY_KP_DIVIDE:
+			return _KP_DIVIDE
+		KEY_KP_SUBTRACT:
+			return _KP_SUBTRACT
+		KEY_KP_PERIOD:
+			return _KP_PERIOD
+		KEY_KP_ADD:
+			return _KP_ADD
+		KEY_KP_0:
+			return _KP_0
+		KEY_KP_1:
+			return _KP_1
+		KEY_KP_2:
+			return _KP_2
+		KEY_KP_3:
+			return _KP_3
+		KEY_KP_4:
+			return _KP_4
+		KEY_KP_5:
+			return _KP_5
+		KEY_KP_6:
+			return _KP_6
+		KEY_KP_7:
+			return _KP_7
+		KEY_KP_8:
+			return _KP_8
+		KEY_KP_9:
+			return _KP_9
+		KEY_MENU:
+			return _MENU
+		KEY_HYPER:
+			return _HYPER
+		KEY_HELP:
+			return _HELP
+		KEY_BACK:
+			return _BACK
+		KEY_FORWARD:
+			return _FORWARD
+		KEY_STOP:
+			return _STOP
+		KEY_REFRESH:
+			return _REFRESH
+		KEY_VOLUMEDOWN:
+			return _VOLUMEDOWN
+		KEY_VOLUMEMUTE:
+			return _VOLUMEMUTE
+		KEY_VOLUMEUP:
+			return _VOLUMEUP
+		KEY_MEDIAPLAY:
+			return _MEDIAPLAY
+		KEY_MEDIASTOP:
+			return _MEDIASTOP
+		KEY_MEDIAPREVIOUS:
+			return _MEDIAPREVIOUS
+		KEY_MEDIANEXT:
+			return _MEDIANEXT
+		KEY_MEDIARECORD:
+			return _MEDIARECORD
+		KEY_HOMEPAGE:
+			return _HOMEPAGE
+		KEY_FAVORITES:
+			return _FAVORITES
+		KEY_SEARCH:
+			return _SEARCH
+		KEY_STANDBY:
+			return _STANDBY
+		KEY_OPENURL:
+			return _OPENURL
+		KEY_LAUNCHMAIL:
+			return _LAUNCHMAIL
+		KEY_LAUNCHMEDIA:
+			return _LAUNCHMEDIA
+		KEY_LAUNCH0:
+			return _LAUNCH0
+		KEY_LAUNCH1:
+			return _LAUNCH1
+		KEY_LAUNCH2:
+			return _LAUNCH2
+		KEY_LAUNCH3:
+			return _LAUNCH3
+		KEY_LAUNCH4:
+			return _LAUNCH4
+		KEY_LAUNCH5:
+			return _LAUNCH5
+		KEY_LAUNCH6:
+			return _LAUNCH6
+		KEY_LAUNCH7:
+			return _LAUNCH7
+		KEY_LAUNCH8:
+			return _LAUNCH8
+		KEY_LAUNCH9:
+			return _LAUNCH9
+		KEY_LAUNCHA:
+			return _LAUNCHA
+		KEY_LAUNCHB:
+			return _LAUNCHB
+		KEY_LAUNCHC:
+			return _LAUNCHC
+		KEY_LAUNCHD:
+			return _LAUNCHD
+		KEY_LAUNCHE:
+			return _LAUNCHE
+		KEY_LAUNCHF:
+			return _LAUNCHF
+		KEY_UNKNOWN:
+			return _UNKNOWN
+		KEY_SPACE:
+			return _SPACE
+		KEY_EXCLAM:
+			return _EXCLAM
+		KEY_QUOTEDBL:
+			return _QUOTEDBL
+		KEY_NUMBERSIGN:
+			return _NUMBERSIGN
+		KEY_DOLLAR:
+			return _DOLLAR
+		KEY_PERCENT:
+			return _PERCENT
+		KEY_AMPERSAND:
+			return _AMPERSAND
+		KEY_APOSTROPHE:
+			return _APOSTROPHE
+		KEY_PARENLEFT:
+			return _PARENLEFT
+		KEY_PARENRIGHT:
+			return _PARENRIGHT
+		KEY_ASTERISK:
+			return _ASTERISK
+		KEY_PLUS:
+			return _PLUS
+		KEY_COMMA:
+			return _COMMA
+		KEY_MINUS:
+			return _MINUS
+		KEY_PERIOD:
+			return _PERIOD
+		KEY_SLASH:
+			return _SLASH
+		KEY_0:
+			return _0
+		KEY_1:
+			return _1
+		KEY_2:
+			return _2
+		KEY_3:
+			return _3
+		KEY_4:
+			return _4
+		KEY_5:
+			return _5
+		KEY_6:
+			return _6
+		KEY_7:
+			return _7
+		KEY_8:
+			return _8
+		KEY_9:
+			return _9
+		KEY_COLON:
+			return _COLON
+		KEY_SEMICOLON:
+			return _SEMICOLON
+		KEY_LESS:
+			return _LESS
+		KEY_EQUAL:
+			return _EQUAL
+		KEY_GREATER:
+			return _GREATER
+		KEY_QUESTION:
+			return _QUESTION
+		KEY_AT:
+			return _AT
+		KEY_A:
+			return _A
+		KEY_B:
+			return _B
+		KEY_C:
+			return _C
+		KEY_D:
+			return _D
+		KEY_E:
+			return _E
+		KEY_F:
+			return _F
+		KEY_G:
+			return _G
+		KEY_H:
+			return _H
+		KEY_I:
+			return _I
+		KEY_J:
+			return _J
+		KEY_K:
+			return _K
+		KEY_L:
+			return _L
+		KEY_M:
+			return _M
+		KEY_N:
+			return _N
+		KEY_O:
+			return _O
+		KEY_P:
+			return _P
+		KEY_Q:
+			return _Q
+		KEY_R:
+			return _R
+		KEY_S:
+			return _S
+		KEY_T:
+			return _T
+		KEY_U:
+			return _U
+		KEY_V:
+			return _V
+		KEY_W:
+			return _W
+		KEY_X:
+			return _X
+		KEY_Y:
+			return _Y
+		KEY_Z:
+			return _Z
+		KEY_BRACKETLEFT:
+			return _BRACKETLEFT
+		KEY_BACKSLASH:
+			return _BACKSLASH
+		KEY_BRACKETRIGHT:
+			return _BRACKETRIGHT
+		KEY_ASCIICIRCUM:
+			return _ASCIICIRCUM
+		KEY_UNDERSCORE:
+			return _UNDERSCORE
+		KEY_QUOTELEFT:
+			return _QUOTELEFT
+		KEY_BRACELEFT:
+			return _BRACELEFT
+		KEY_BAR:
+			return _BAR
+		KEY_BRACERIGHT:
+			return _BRACERIGHT
+		KEY_ASCIITILDE:
+			return _ASCIITILDE
+		KEY_YEN:
+			return _YEN
+		KEY_SECTION:
+			return _SECTION
+		KEY_GLOBE:
+			return _GLOBE
+		KEY_KEYBOARD:
+			return _KEYBOARD
+		KEY_JIS_EISU:
+			return _JIS_EISU
+		KEY_JIS_KANA:
+			return _JIS_KANA
 		_:
 			return ""
