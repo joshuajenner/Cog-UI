3911d93af37169de8165f986fb566a254863f620
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-04-07 15:44:43 +0800

    switched to physics process for performance

--- addons/cog_ui/classes/system_clock/system_clock.gd
+++ addons/cog_ui/classes/system_clock/system_clock.gd
@@ -2,7 +2,7 @@ class_name SystemClock
 extends Label
 
 
-func _process(delta) -> void:
+func _physics_process(_delta) -> void:
 	var time_dict : Dictionary = Time.get_time_dict_from_system()
 	var display : String = ("%02d" % time_dict.hour) + ":" + ("%02d" % time_dict.minute)
 	
