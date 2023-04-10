2b34eb7d858ca4f71262d334e72d605776b87e13
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-04-10 16:44:03 +0800

    updated signal naming

--- addons/cog_ui/autoloads/audio_bus_manager.gd
+++ addons/cog_ui/autoloads/audio_bus_manager.gd
@@ -1,15 +1,12 @@
 extends Node
 
 
-signal audio_settings_changed
-signal audio_settings_loaded
-signal audio_settings_saved
+signal bus_layout_changed
+signal bus_layout_loaded
+signal bus_layout_saved
 
-
-const USER_PATH: String = "user://user_audio_settings.tres"
+const USER_PATH: String = "user://user_bus_layout.tres"
 const DEFAULT_PATH: String = "res://default_bus_layout.tres"
-
-var has_unsaved_changes: bool = false
 
 
 func _ready():
@@ -32,19 +29,10 @@ func load_user_bus_layout() -> void:
 func save_user_bus_layout() -> void:
 	var bus_layout: AudioBusLayout = AudioServer.generate_bus_layout()
 	ResourceSaver.save(bus_layout, USER_PATH)
+	bus_layout_saved.emit()
 
 
 func _load_bus_layout(path: String) -> void:
 	var file_layout: AudioBusLayout = ResourceLoader.load(path)
 	AudioServer.set_bus_layout(file_layout)
-	audio_settings_loaded.emit()
-
-
-func set_as_unsaved() -> void:
-	has_unsaved_changes = true
-	audio_settings_changed.emit()
-
-
-func set_as_saved() -> void:
-	has_unsaved_changes = false
-	audio_settings_saved.emit()
+	bus_layout_loaded.emit()
--- examples/volume_settings/volume_settings.gd
+++ examples/volume_settings/volume_settings.gd
@@ -3,13 +3,12 @@ extends Control
 
 @onready var save = %Save
 @onready var cancel = %Cancel
-@onready var reset = %Reset
 
 
 func _ready():
-	AudioBusManager.connect("audio_settings_saved", disable_buttons)
-	AudioBusManager.connect("audio_settings_changed", enable_buttons)
-	AudioBusManager.connect("audio_settings_loaded", enable_buttons)
+	AudioBusManager.connect("bus_layout_saved", disable_buttons)
+	AudioBusManager.connect("bus_layout_changed", enable_buttons)
+	AudioBusManager.connect("bus_layout_loaded", enable_buttons)
 	disable_buttons()
 
 
@@ -25,14 +24,12 @@ func enable_buttons():
 
 func _on_save_pressed():
 	AudioBusManager.save_user_bus_layout()
-	AudioBusManager.set_as_saved()
 
 
 func _on_cancel_pressed():
 	AudioBusManager.load_user_bus_layout()
-	AudioBusManager.set_as_saved()
+	AudioBusManager.audio_settings_saved.emit()
 
 
 func _on_reset_pressed():
 	AudioBusManager.load_default_bus_layout()
-	AudioBusManager.set_as_unsaved()
