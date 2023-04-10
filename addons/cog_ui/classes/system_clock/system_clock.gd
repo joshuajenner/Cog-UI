3cd6e3d41352a982cc23a9b3d8bb32af3177e4f6
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-04-10 16:43:14 +0800

    cleaned code

--- addons/cog_ui/classes/system_clock/system_clock.gd
+++ addons/cog_ui/classes/system_clock/system_clock.gd
@@ -3,7 +3,7 @@ extends Label
 
 
 func _physics_process(_delta) -> void:
-	var time_dict : Dictionary = Time.get_time_dict_from_system()
-	var display : String = ("%02d" % time_dict.hour) + ":" + ("%02d" % time_dict.minute)
+	var time_dict: Dictionary = Time.get_time_dict_from_system()
+	var display: String = ("%02d" % time_dict.hour) + ":" + ("%02d" % time_dict.minute)
 	
 	text = display
