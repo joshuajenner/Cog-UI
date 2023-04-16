d9c0453afd3d8c6018b0b40046dcd657a16adae4
Joshua Jenner <joshua-jenner@hotmail.com>
2023-04-16 16:22:12 +0800

    mouse button now displays label

--- addons/cog_ui/classes/input_map_button/input_map_button.gd
+++ addons/cog_ui/classes/input_map_button/input_map_button.gd
@@ -1,15 +1,17 @@
 class_name InputMapButton
 extends Button
 
-@export var action : String
-@export var event_index : int = 0
+#@onready var tooltip = $Tooltip
+
+@export var action: String
+@export var event_index: int = 0
 @export var kbm_can_assign: bool = true
 @export var joypad_can_assign: bool = false
 
-const INPUT_REQUEST : String = "Press any key"
+const INPUT_REQUEST: String = "Press any key"
 
-var event_label : String = ""
-var waiting_for_assign : bool = false
+var event_label: String = ""
+var waiting_for_assign: bool = false
 
 
 func _ready():
@@ -23,7 +25,7 @@ func _ready():
 func get_events_from_action():
 	var events : Array[InputEvent] = InputMap.action_get_events(action)
 	if events.size() > event_index:
-		set_key_label(events[event_index])
+		set_event_label(events[event_index])
 		disabled = false
 	elif events.size() < event_index:
 		disabled = true
@@ -36,13 +38,12 @@ func _input(event):
 		if event.is_action_pressed("ui_cancel"):
 			text = event_label
 			waiting_for_assign = false
-		elif event is InputEventKey and event.is_pressed():
+		elif event is InputEventKey or event is InputEventMouseButton and event.is_pressed():
 			assign_event_to_action(event)
-			set_key_label(event)
+			set_event_label(event)
 			text = event_label
+			get_viewport().set_input_as_handled()
 			release_focus()
-		elif event is InputEventMouseButton:
-			print(event.as_text())
 
 
 func assign_event_to_action(new_event: InputEvent):
@@ -60,25 +61,30 @@ func assign_event_to_action(new_event: InputEvent):
 	waiting_for_assign = false
 
 
-func set_key_label(event : InputEvent):
-	var event_keycode: int
-	var event_text: String
-	
-	if event.physical_keycode != 0:
-		event_keycode = event.physical_keycode
-		event_text = event.as_text_physical_keycode()
-	elif event.keycode != 0:
-		event_keycode = event.keycode
-		event_text = event.as_text_keycode()
+func set_event_label(event : InputEvent) -> void:
+	if event is InputEventKey:
+		var event_keycode: int
+		var event_text: String
 		
-	var new_label : String = InputMapManager.get_custom_key(event_keycode)
-	if new_label != "":
-		event_label = new_label
-		pass
+		if event.physical_keycode != 0:
+			event_keycode = event.physical_keycode
+			event_text = event.as_text_physical_keycode()
+		elif event.keycode != 0:
+			event_keycode = event.keycode
+			event_text = event.as_text_keycode()
+			
+		var new_label : String = InputMapManager.get_custom_key(event_keycode)
+		if new_label != "":
+			event_label = new_label
+			pass
 		
-	event_label = event_text
+		event_label = event_text
+		
+	elif event is InputEventMouseButton:
+		event_label = event.as_text()
 
 
 func _on_pressed():
-	text = INPUT_REQUEST
-	waiting_for_assign = true
+	if not waiting_for_assign:
+		text = INPUT_REQUEST
+		waiting_for_assign = true
