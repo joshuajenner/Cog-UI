f267acba3a59a9178003c0b893252f354140308f
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2024-03-22 19:55:12 +0800

    Cleanup

--- examples/input_settings/input_settings.gd
+++ examples/input_settings/input_settings.gd
@@ -5,16 +5,11 @@ extends Control
 @onready var cancel_bind = %CancelBind
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	InputMapManager.edit_completed.connect(_on_edit_completed)
 	InputMapManager.edit_requested.connect(_on_edit_requested)
 	InputMapManager.edit_canceled.connect(_on_edit_completed)
 	_show_instructions(false)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_edit_requested():
