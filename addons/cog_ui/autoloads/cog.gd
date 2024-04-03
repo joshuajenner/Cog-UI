9e381878a77ae647e069e14b5616a32f34af7baf
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2024-04-03 18:03:43 +0800

    Added the main Cog class

---
+++ addons/cog_ui/autoloads/cog.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var audio: CogAudio = $Audio
+var controls: CogControls = $Controls
+
+var are_changes_pending: bool = false
+
+
+func _ready():
+	pass
+
+
+func save_all():
+	pass
+
+
--- examples/main_menu/main_menu.gd
+++ examples/main_menu/main_menu.gd
@@ -4,6 +4,5 @@ extends Control
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://examples/settings/settings.tscn")
 
-
 func _on_quit_pressed():
 	get_tree().quit()
