213b871206e73667395838b0c36703579c6f5b2e
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2024-04-11 00:01:18 +0800

    Added Cog Autoload

--- addons/cog_ui/autoloads/cog_audio.gd
+++ addons/cog_ui/autoloads/cog_audio.gd
@@ -2,9 +2,9 @@ class_name CogAudio
 extends Node
 
 
-signal bus_layout_changed
-signal bus_layout_loaded
-signal bus_layout_saved
+signal changed
+signal loaded
+signal saved
 
 
 func _ready():
@@ -13,24 +13,24 @@ func _ready():
 	if file_exists:
 		load_user_bus_layout()
 	else:
-		save_user_bus_layout()
+		save()
 
 
 func load_default_bus_layout() -> void:
-	_load_bus_layout(CogPaths.BUS_LAYOUT_DEFAULT_PATH)
+	_load(CogPaths.BUS_LAYOUT_DEFAULT_PATH)
 
 
 func load_user_bus_layout() -> void:
-	_load_bus_layout(CogPaths.BUS_LAYOUT_USER_PATH)
+	_load(CogPaths.BUS_LAYOUT_USER_PATH)
 
 
-func save_user_bus_layout() -> void:
+func save() -> void:
 	var bus_layout: AudioBusLayout = AudioServer.generate_bus_layout()
 	ResourceSaver.save(bus_layout, CogPaths.BUS_LAYOUT_USER_PATH)
-	bus_layout_saved.emit()
+	saved.emit()
 
 
-func _load_bus_layout(path: String) -> void:
+func _load(path: String) -> void:
 	var file_layout: AudioBusLayout = ResourceLoader.load(path)
 	AudioServer.set_bus_layout(file_layout)
-	bus_layout_loaded.emit()
+	loaded.emit()
