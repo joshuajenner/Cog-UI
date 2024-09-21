5b4db595cff636f6886c9d63e535108d4e8668bf
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2024-09-21 22:13:03 +0800

    Removed unused paths

--- addons/cog/cog.gd
+++ addons/cog/cog.gd
@@ -4,11 +4,6 @@ extends EditorPlugin
 
 
 # Edit the paths as needed
-const BUS_LAYOUT_USER_PATH: String = "user://user_bus_layout.cfg"
-const BUS_LAYOUT_DEFAULT_PATH: String = "res://default_bus_layout.tres"
-
-const INPUT_MAP_USER_PATH: String = "user://user_input_map.cfg"
-
 const VIDEO_SETTINGS_USER_PATH: String = "user://video_settings_user.cfg"
 const VIDEO_SETTINGS_DEFAULT_PATH: String = "user://video_settings_default.cfg"
 
